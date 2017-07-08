7c23ef2f903cd7369539246bc4519edf502fc934
Juan D. Acosta <juandacorias@gmail.com>
2017-07-08 08:50:50 -0500

    Working version with config file

--- test.js
+++ test.js
@@ -1,14 +1,10 @@
-/*
-START SERVER WITH
-browser-sync start --server --files index.html style.css
-*/
-
 const fs = require('fs');
 const path = require('path');
 require('dotenv').config();
 const fetch = require('node-fetch');
 const showdown = require('showdown');
 const htmlAutoFormat = require('js-beautify').html;
+const embed = require('embed-video');
 
 const converter = new showdown.Converter();
 
@@ -16,13 +12,18 @@ const readmeURL =
   'https://raw.githubusercontent.com/freecodecamp/mail-for-good/master/README.md';
 const contributorsURL =
   'https://api.github.com/repos/freeCodeCamp/mail-for-good/contributors';
+const repoFullName = 'freecodecamp/mail-for-good';
 let rawReadme;
+
+const repoConfig = require('./.osfg-dir-config');
 
 fetch(readmeURL)
   .then(res => res.text())
   // Fetch Contributors
   .then(text => {
-    rawReadme = text;
+    // rawReadme = text;
+    rawReadme = require('./NONPROFIT');
+
     /* Header Inclusion necessary for the
         GitHub API https://developer.github.com/v3/#user-agent-required */
     const options = {
@@ -37,13 +38,10 @@ fetch(readmeURL)
     /*
       Building the HTML Web Page from the Fetched Data
     */
-    const contributors = buildContributorHtml(contributorsData);
-    const body = converter.makeHtml(rawReadme);
-    const repoName = 'freecodecamp/mail-for-good';
-    const page = buildPage(repoName, body, contributors);
-
+    const repoLink = 'https://www.github.com/freecodecamp/mail-for-good';
+    const contributors = buildContributorsHtml(contributorsData);
+    const page = buildPage(repoConfig, repoLink, contributors);
     const formattedPage = htmlAutoFormat(page, { indent_size: 2 });
-
     /*
       Processing the File
     */
@@ -56,34 +54,67 @@ fetch(readmeURL)
 /*
   Building WebPage
 */
-function buildContributorHtml(contributors) {
+function buildContributorsHtml(contribData) {
   let html = '';
-  contributors.forEach(c => {
+  contribData.forEach(contributor => {
     html += `
     <div class="contributor">
-      <a class="contributor-link" href="${c.html_url}">
-        <img class="contributor-img" src="${c.avatar_url}"/>
+      <a class="contributor-link" href="${contributor.html_url}" target="_blank">
+        <img class="contributor-img" src="${contributor.avatar_url}"/>
       </a>
     </div>`;
   });
   return html;
 }
 
-function buildPage(name, body, contributors) {
+function buildPage(repoConfig, repoLink, contributors) {
+  const { title, description, demoVideo, liveDemo, body } = repoConfig;
+  const screenshot = null;
   return `
     <!DOCTYPE html>
     <html>
       <header>
         <link rel="stylesheet" href="./style.css">
+        <!-- Font Awesome -->
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
       </header>
       <body>
         <div class="wrapper">
-          <div class="fcc-banner">
-            <img src="https://cdn.glitch.com/f9a9063e-4605-4536-942e-6a948a65598e%2Ffcc-logo-white.png?1491457226808"/>
-          </div>
+          <a href="https://www.freecodecamp.org">
+            <div class="fcc-banner">
+              <img src="https://cdn.glitch.com/f9a9063e-4605-4536-942e-6a948a65598e%2Ffcc-logo-white.png?1491457226808"/>
+            </div>
+          </a>
           <div class="content-container">
-            <h1 class="repo-name">${name}</h1>
-            ${body}
+            <h1 class="repo-name">${title}</h1>
+            <div class="project-description">
+              ${description ? converter.makeHtml(description) : ''}
+            </div>
+            ${demoVideo
+              ? `<div class="project-video">
+                  <div class="video-container">
+                    ${embed(demoVideo, { attr: { width: 640, height: 360 } })}
+                  </div>
+                </div>`
+              : ''}
+            ${screenshot ? 
+              `<img src="${screenshot}"`
+              : ''}
+            <div class="buttons-container">
+              <a href="${liveDemo}" target="_blank">
+                <button>
+                  Live Demo 
+                  <i class="fa fa-cube" aria-hidden="true"></i>
+                </button></a>
+              <a href="${repoLink}" target="_blank">
+                <button>
+                  Code Repo
+                  <i class="fa fa-code" aria-hidden="true"></i>
+                </button></a>
+            </div>
+            <div class="body-container">
+              ${body ? converter.makeHtml(body) : ''}
+            </div>
             <h2>Contributors</h2>
             <div class="contributors">
               ${contributors}
