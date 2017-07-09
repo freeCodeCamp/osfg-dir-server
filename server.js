a48a58e3523af51f65d9d9124508c044e5de4b5f
Juan D. Acosta <juandacorias@gmail.com>
2017-07-08 20:00:24 -0500

    Moved to nonprofits.css

--- server.js
+++ server.js
@@ -159,7 +159,7 @@ function buildPage(repoConfig, contributors) {
     <!DOCTYPE html>
     <html>
       <header>
-        <link rel="stylesheet" href="../style.css">
+        <link rel="stylesheet" href="../nonprofits.css">
         <!-- Font Awesome -->
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
       </header>
